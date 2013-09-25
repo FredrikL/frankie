b03e17033f4153594e02f1705319f0b7ad1615bc
Fredrik Leijon <Fredrik Leijon>
2013-09-25 15:09:03 +0200

    read input from client

--- connection.hpp
+++ connection.hpp
@@ -9,7 +9,8 @@ namespace frankie {
 
 	using boost::asio::ip::tcp;
 
-	class Connection {
+	class Connection :
+		public std::enable_shared_from_this<Connection> {
 	public:
 		static std::shared_ptr<Connection> create(boost::asio::io_service& service) {
 			return std::shared_ptr<Connection>(new Connection(service));
@@ -20,12 +21,12 @@ namespace frankie {
 		}
 
 		void handle_request() {
-			msg = "Hai!";
-
-			boost::asio::async_write(_socket, boost::asio::buffer(msg),
-				boost::bind(&Connection::handle_write, this,
-				boost::asio::placeholders::error, 
-				boost::asio::placeholders::bytes_transferred));
+			std::cout << "handle_request" << std::endl;
+			boost::asio::async_read_until(_socket, buffer,
+				"\r\n\r\n",
+				boost::bind(&Connection::handle_read, shared_from_this(), 
+					boost::asio::placeholders::error, 
+					boost::asio::placeholders::bytes_transferred));
 		}
 
 	private:
@@ -33,9 +34,33 @@ namespace frankie {
 
 		}
 
-		void handle_write(const boost::system::error_code&, size_t) {
+		void handle_read(const boost::system::error_code& error, std::size_t bytes) {
+			std::cout << "handle_read" << std::endl;
+			std::cout << error << std::endl;
+			if(!error) {
+				std::cout << "no error" << std::endl;
+
+				auto bufs = buffer.data();
+				std::string input(boost::asio::buffers_begin(bufs), boost::asio::buffers_begin(bufs) + bytes);
+				buffer.consume(bytes);
+
+				// magic?!
+				std::cout << input << std::endl;
+
+				msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 100\r\n\r\nHai\r\n\r\n";
+
+				boost::asio::async_write(_socket, boost::asio::buffer(msg),
+					boost::bind(&Connection::handle_write, shared_from_this(),
+					boost::asio::placeholders::error, 
+					boost::asio::placeholders::bytes_transferred));
+			}
 		}
 
+		void handle_write(const boost::system::error_code&, size_t) {
+			std::cout << "handle_write" << std::endl;
+		}
+
+		boost::asio::streambuf buffer;
 		tcp::socket _socket;
 		std::string msg;
 	};
