4307c33b3eb2916ac6b2141c876628cc515c8649
Fredrik Leijon <Fredrik Leijon>
2013-09-30 15:21:54 +0200

    test default response code

--- test/response_test.cpp
+++ test/response_test.cpp
@@ -7,11 +7,16 @@ using namespace frankie;
 go_bandit([](){
 	describe("response:", [](){
 
+		it("should have a default status of 200", [](){
+			Response resp("foo");
+			AssertThat(resp.status(), Equals(200u));
+		});
+
+
 		it("NotFoundResponse should have status 404", [](){
 			NotFoundResponse resp;
 			AssertThat(resp.status(), Equals(404u));
 		});
-
 	});
 
 });
